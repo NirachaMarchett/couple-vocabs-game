4280800359bd713b52c564669c18f9409391d323
Jonathan <jonathan.schernus@gmail.com>
2023-09-27 12:12:23 +0200

    setup react project

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+}
 
-    return config;
-  },
-  images: {
-    domains: ["edamam-product-images.s3.amazonaws.com"],
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
